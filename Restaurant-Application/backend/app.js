9b134d9cc132d923a303f660dac6ac90aedeee8c
Vandan Kambodi <vandankambodi27@gmail.com>
2025-04-03 20:02:41 +0530

    Update

--- Restaurant-Application/backend/app.js
+++ Restaurant-Application/backend/app.js
@@ -1,27 +1,31 @@
 import express from "express";
-import cors from "cors"
 import dotenv from "dotenv";
-import { dbConnection } from "./database/dbconnection.js";
-import { errorMiddleware } from "./error/error.js";  
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
 import reservationRouter from "./routes/reservationRoute.js";
+import { dbConnection } from "./database/dbConnection.js";
 
+const app = express();
+dotenv.config({ path: "./config.env" });
 
-const app = express()
-dotenv.config({path:"./config/config.env"})
-
-app.use(cors({
+app.use(
+  cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["POST"],
-    credentials: true
-}))
-
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use('/api/v1/reservation', reservationRouter)
+app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/v1/reservation", reservationRouter);
+app.get("/", (req, res, next)=>{return res.status(200).json({
+  success: true,
+  message: "HELLO WORLD AGAIN"
+})})
 
 dbConnection();
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 export default app;
--- Restaurant-Application/backend/models/reservationSchema.js
+++ Restaurant-Application/backend/models/reservationSchema.js
@@ -2,49 +2,37 @@ import mongoose from "mongoose";
 import validator from "validator";
 
 const reservationSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: [true, "Name is required"],
-        minLength: [3, "First Name must contains atleast 3 characters!"],
-        maxLength: [30, "First Name cannot exceed 30 characters!"],
-        trim: true,
-    },
-    lastName: {
-        type: String,
-        required: [true, "Name is required"],
-        minLength: [3, "Last Name must contains atleast 3 characters!"],
-        maxLength: [30, "Last Name cannot exceed 30 characters!"],
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: [true, "Email is required"],
-        validate: {
-            validator: (value) => validator.isEmail(value),
-            message: "Invalid email format",
-        }, 
-    },
-    phone: {
-        type: String,
-        required: [true, "Phone number is required"],
-        minLength: [10, "Phone number must contain only 10 digits!"],
-        maxLength: [10, "Phone number must contain only 10 digits!"],
-    },
-    date: {
-        type: Date,
-        required: [true, "Reservation date is required"],
-    },
-    time: {
-        type: String,
-        required: [true, "Reservation time is required"],
-    },
-    // numberOfGuests: {
-    //     type: Number,
-    //     required: [true, "Number of guests is required"],
-    //     min: [1, "At least one guest is required"],
-    // },
+  firstName: {
+    type: String,
+    required: true,
+    minLength: [3, "First name must be of at least 3 Characters."],
+    maxLength: [30, "First name cannot exceed 30 Characters."],
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minLength: [3, "Last name must be of at least 3 Characters."],
+    maxLength: [30, "Last name cannot exceed 30 Characters."],
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: [validator.isEmail, "Provide a valid email"],
+  },
+  phone: {
+    type: String,
+    required: true,
+    minLength: [11, "Phone number must contain 11 Digits."],
+    maxLength: [11, "Phone number must contain 11 Digits."],
+  },
 });
 
-
 export const Reservation = mongoose.model("Reservation", reservationSchema);
-export default mongoose.model("Reservation", reservationSchema);
